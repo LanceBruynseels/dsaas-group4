ba0c4a8482b3b97c2deb3a3752582ec50406454a
Rohan Bhattaram <rohan.bhattaram@student.kuleuven.be>
2024-10-24 15:50:52 +0200

    CI pipeline with cypress tests

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -32,32 +32,29 @@
 //   })
 // })
 
+describe('Cypress test', () => {
+  it('clicking database.new ', () => {
+    cy.visit('https://dsaas-group4.vercel.app/')
+
+    cy.contains('database.new').click()
+
+  })
+})
+
 // describe('My First Test', () => {
-//   it('clicking "type" navigates to a new url', () => {
+//   it('Gets, types and asserts', () => {
 //     cy.visit('https://example.cypress.io')
 //
 //     cy.contains('type').click()
 //
 //     // Should be on a new URL which
 //     // includes '/commands/actions'
-//     cy.url().should('include', '/commands/actions')    // check if expected URL is correct
+//     cy.url().should('include', '/commands/actions')
+//
+//     // Get an input, type into it
+//     cy.get('.action-email').type('fake@email.com')
+//
+//     //  Verify that the value has been updated
+//     cy.get('.action-email').should('have.value', 'fake@email.com')
 //   })
 // })
-
-describe('My First Test', () => {
-  it('Gets, types and asserts', () => {
-    cy.visit('https://example.cypress.io')
-
-    cy.contains('type').click()
-
-    // Should be on a new URL which
-    // includes '/commands/actions'
-    cy.url().should('include', '/commands/actions')
-
-    // Get an input, type into it
-    cy.get('.action-email').type('fake@email.com')
-
-    //  Verify that the value has been updated
-    cy.get('.action-email').should('have.value', 'fake@email.com')
-  })
-})
