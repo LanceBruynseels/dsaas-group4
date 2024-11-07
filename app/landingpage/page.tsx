f122483c5754d9c49bc141d16adff40144417af3
manuel Pollet <manuel.pollet@student.kuleuven.be>
2024-11-07 15:34:32 +0100

    LandingPage linked buttons (version2)

--- app/landingpage/page.tsx
+++ app/landingpage/page.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Index() {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-pink-100 text-center relative">
+        <div className="flex flex-col items-center justify-center min-h-screen text-center relative">
             <div className="mb-5">
                 {/* Replace with the actual path to your butterfly logo */}
                 <img src="vlinder.png" alt="Vlinder Logo" className="w-24 h-auto mx-auto" />
@@ -10,9 +10,13 @@ export default function Index() {
             </div>
 
             <div className="flex space-x-4 mt-5">
-                <button className="bg-pink-400 hover:bg-pink-500 text-white py-2 px-6 rounded-full transition duration-300">LOGIN</button>
-                <button className="bg-pink-400 hover:bg-pink-500 text-white py-2 px-6 rounded-full transition duration-300">REGISTRATIE</button>
-            </div>
+                <a href = "http://localhost:3000/sign-up">
+                <button  className="bg-pink-400 hover:bg-pink-500 text-white py-2 px-6 rounded-full transition duration-300">LOGIN</button>
+                </a>
+                <a href = "http://localhost:3000/sign-in">
+                    <button className="bg-pink-400 hover:bg-pink-500 text-white py-2 px-6 rounded-full transition duration-300">REGISTRATIE</button>
+                </a>
+                </div>
 
 
         </div>
