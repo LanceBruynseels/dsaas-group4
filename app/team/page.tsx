b53e16bcfa10343047231c247d0ca9a5f6098e57
Manuel Pollet <manuel.pollet@student.kuleuven.be>
2024-11-17 19:38:13 +0100

    about us page - Finish just needs right colors-  (version 4)

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -18,7 +18,7 @@ const TeamSection = () => {
     const [currentDescription, setCurrentDescription] = useState(teamMembers[0].description);
 
     return (
-        <section className="bg-white w-screen h-screen flex flex-col items-center overflow-hidden justify-start gap-8 ">
+        <section className="bg-[#FDFDFD] w-screen h-screen flex flex-col items-center overflow-hidden justify-start gap-8 m ">
             <div className="flex flex-col h-1/6">
                 <h2 className="text-center sm:text-lg md:text-3xl lg:text-6xl xl:text-8xl text-red-950 ">
                     Meet our team
@@ -26,13 +26,13 @@ const TeamSection = () => {
 
             </div>
             <div className="flex flex-row w-screen h-3/6 justify-center gap-8">
-                <div className="flex w-3/12 h-full aspect-square border-2 border-red-950 rounded-2xl">
-                    <img className="w-full h-full" src={currentImage}/>
+                <div className="flex w-3/12 h-full aspect-square">
+                    <img className="w-full h-full  border-2 border-red-950 rounded-2xl " src={currentImage}/>
                 </div>
                 <div className="flex flex-col w-5/12 h-full gap-4 p-4">
-                    <p className="text-5xl bg-purple-600"> {currentName}</p>
-                    <p className="text-3xl"> {currentFunction}</p>
-                    <p className="text-1xl"> {currentDescription}</p>
+                    <p className="text-5xl text-[#6f5956] bg-[#d4c1f6]"> {currentName}</p>
+                    <p className="text-3xl text-[#6f5956]"> {currentFunction}</p>
+                    <p className="text-1xl text-[#6f5956]"> {currentDescription}</p>
                 </div>
             </div>
             <div className="flex  flex-row w-screen h-1/6 gap-[5%] justify-center">
