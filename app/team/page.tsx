d779eb6c09396c9fec950e5330c43fd1c5b6ee9c
Manuel Pollet <manuel.pollet@student.kuleuven.be>
2024-11-17 19:22:29 +0100

    about us page - extra push bc git fail-  (version 3)

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -3,16 +3,19 @@ import React, { useState } from 'react';
 
 
 const teamMembers = [
-    {name: 'NAME', function: 'FUNCTION', description: 'description', picture: 'vlinder.png'},
-    {name: 'NAME', function: 'FUNCTION', description: 'description', picture: 'mock-picture.webp'},
-    {name: 'NAME', function: 'FUNCTION', description: 'description', picture: 'vlinder.png'},
-    {name: 'NAME', function: 'FUNCTION', description: 'description', picture: 'vlinder.png'},
-    {name: 'NAME', function: 'FUNCTION', description: 'description', picture: 'vlinder.png'},
-    {name: 'NAME', function: 'FUNCTION', description: 'description', picture: 'vlinder.png'},
+    {name: 'NAME ONE', function: 'FUNCTION', description: 'description', picture: 'vlinder.png'},
+    {name: 'NAME TWO', function: 'FUNCTION', description: 'description', picture: 'mock-picture.webp'},
+    {name: 'NAME THREE', function: 'FUNCTION', description: 'description', picture: 'vlinder.png'},
+    {name: 'NAME FOUR', function: 'FUNCTION', description: 'description', picture: 'vlinder.png'},
+    {name: 'NAME Five', function: 'FUNCTION', description: 'description', picture: 'vlinder.png'},
+    {name: 'NAME SIX', function: 'FUNCTION', description: 'description', picture: 'vlinder.png'},
 ];
 
 const TeamSection = () => {
     const [currentImage, setCurrentImage] = useState(teamMembers[0].picture);
+    const [currentName, setCurrentName] = useState(teamMembers[0].name);
+    const [currentFunction, setCurrentFunction] = useState(teamMembers[0].function);
+    const [currentDescription, setCurrentDescription] = useState(teamMembers[0].description);
 
     return (
         <section className="bg-white w-screen h-screen flex flex-col items-center overflow-hidden justify-start gap-8 ">
@@ -20,29 +23,41 @@ const TeamSection = () => {
                 <h2 className="text-center sm:text-lg md:text-3xl lg:text-6xl xl:text-8xl text-red-950 ">
                     Meet our team
                 </h2>
-                <h2 className="text-center sm:text-lg md:text-lg lg:text-xl xl:text-xl text-red-950 ">
-                    Meet our talented team of bright engineers that made this innovative project happen
-                </h2>
+
             </div>
-            <div className="flex flex-row w-screen h-3/6 bg-red-500 justify-center gap-8">
-                <div className="flex w-3/12 h-full aspect-square bg-yellow-400">
-                    <img src={currentImage}/>
+            <div className="flex flex-row w-screen h-3/6 justify-center gap-8">
+                <div className="flex w-3/12 h-full aspect-square border-2 border-red-950 rounded-2xl">
+                    <img className="w-full h-full" src={currentImage}/>
                 </div>
-                <div className="flex w-5/12 h-full bg-green-300">
-                    nen hele hoop text habibi
+                <div className="flex flex-col w-5/12 h-full gap-4 p-4">
+                    <p className="text-5xl bg-purple-600"> {currentName}</p>
+                    <p className="text-3xl"> {currentFunction}</p>
+                    <p className="text-1xl"> {currentDescription}</p>
                 </div>
             </div>
-            <div className="flex  flex-row w-screen h-1/6 bg-purple-600 gap-[5%] justify-center">
+            <div className="flex  flex-row w-screen h-1/6 gap-[5%] justify-center">
 
                 {teamMembers.map((member, index) => (
                     <div
                         key={index}
-                        className="flex flex-row items-start bg-red-800 border-2 border-red-950 rounded-lg aspect-square "
+                        className="flex flex-row items-start aspect-square "
                     >
-                        <div className="flex flex-col items-start text-center bg-black"
-                             onMouseEnter={() => setCurrentImage(member.picture)}
-                             onMouseLeave={() => setCurrentImage(member.picture)}>
-                            <img src={member.picture}/>
+                        <div className="flex flex-col items-start text-center"
+                             onMouseEnter={() => {
+                                 setCurrentImage(member.picture);
+                                 setCurrentName(member.name);
+                                 setCurrentFunction(member.function);
+                                 setCurrentDescription(member.description)}}
+
+
+                             onMouseLeave={() => {
+                                 setCurrentImage(member.picture);
+                                 setCurrentName(member.name);
+                                 setCurrentFunction(member.function);
+                                 setCurrentDescription(member.description);
+                             }}>
+
+                            <img className="border-2 border-red-950 rounded-2xl" src={member.picture}/>
                         </div>
                     </div>
                 ))}
