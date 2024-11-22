c8bc33d04412a4432ae45d3e062abd53bd25d841
CantonMonkey <cintauleung@gmail.com>
2024-11-22 11:30:05 +0100

    minor changes

--- app/api/endUser-activation/route.ts
+++ app/api/endUser-activation/route.ts
@@ -1,27 +1,27 @@
-import { createClient } from '@supabase/supabase-js'
-
-const supabase = createClient(supabase_url, anon_key)
-
-// do we need to put getUser in login API? seems it only works when caretaker is logged in,
-// yeah I think so, it's retrieved form a session, but the returned data is from DB
-
-// get user
-const { data: { user } } = await supabase.auth.getUser()
-
-// get caretaker.id from the json returned
-
-// query caretaker.id in table "users"
-
-// return a list of pending users and pass to FE
-
-
-
-
-
-
-
-
-
-
-
-
+// import { createClient } from '@supabase/supabase-js'
+//
+// const supabase = createClient(supabase_url, anon_key)
+//
+// // do we need to put getUser in login API? seems it only works when caretaker is logged in,
+// // yeah I think so, it's retrieved form a session, but the returned data is from DB
+//
+// // get user
+// const { data: { user } } = await supabase.auth.getUser()
+//
+// // get caretaker.id from the json returned
+//
+// // query caretaker.id in table "users"
+//
+// // return a list of pending users and pass to FE
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
