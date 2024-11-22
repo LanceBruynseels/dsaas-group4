c22c85a6c1a4a7b1813c0733ae77dd97bcf1b464
Manuel Pollet <manuel.pollet@student.kuleuven.be>
2024-11-22 15:00:03 +0100

    Payment page - imporved visuals - (version5)

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -3,12 +3,12 @@ import React, { useState } from 'react';
 
 
 const teamMembers = [
-    {name: 'NAME ONE', function: 'FUNCTION', description: 'description', picture: 'vlinder.png'},
-    {name: 'NAME TWO', function: 'FUNCTION', description: 'description', picture: 'mock-picture.webp'},
-    {name: 'NAME THREE', function: 'FUNCTION', description: 'description', picture: 'vlinder.png'},
-    {name: 'NAME FOUR', function: 'FUNCTION', description: 'description', picture: 'vlinder.png'},
-    {name: 'NAME Five', function: 'FUNCTION', description: 'description', picture: 'vlinder.png'},
-    {name: 'NAME SIX', function: 'FUNCTION', description: 'description', picture: 'vlinder.png'},
+    {name: 'Anton Ceusters', function: 'Software Engineer', description: 'description', picture: 'vlinder.png'},
+    {name: 'Datao Liang', function: 'Software Engineer', description: 'description', picture: 'mock-picture.webp'},
+    {name: 'Joran Vleugels', function: 'Software Engineer', description: 'description', picture: 'vlinder.png'},
+    {name: 'Lance Bruynseels', function: 'Software Engineer', description: 'description', picture: 'vlinder.png'},
+    {name: 'Manuel Pollet', function: 'Software Engineer', description: 'description', picture: 'vlinder.png'},
+    {name: 'Rohan Bhattaram', function: 'Software Engineer', description: 'description', picture: 'vlinder.png'},
 ];
 
 const TeamSection = () => {
@@ -18,21 +18,21 @@ const TeamSection = () => {
     const [currentDescription, setCurrentDescription] = useState(teamMembers[0].description);
 
     return (
-        <section className="bg-[#FDFDFD] w-screen h-screen flex flex-col items-center overflow-hidden justify-start gap-8 m ">
+        <section className="bg-white w-screen h-screen flex flex-col items-center overflow-hidden justify-start gap-8">
             <div className="flex flex-col h-1/6">
-                <h2 className="text-center sm:text-lg md:text-3xl lg:text-6xl xl:text-8xl text-red-950 ">
+                <h2 className="text-center w-screen p-4 m-4 font-bold  text-7xl text-white bg-[#d4c1f6] ">
                     Meet our team
                 </h2>
 
             </div>
             <div className="flex flex-row w-screen h-3/6 justify-center gap-8">
                 <div className="flex w-3/12 h-full aspect-square">
-                    <img className="w-full h-full  border-2 border-red-950 rounded-2xl " src={currentImage}/>
+                    <img className="w-full h-full  border-4 border-[#6f5956] rounded-2xl " src={currentImage}/>
                 </div>
-                <div className="flex flex-col w-5/12 h-full gap-4 p-4">
-                    <p className="text-5xl text-[#6f5956] bg-[#d4c1f6]"> {currentName}</p>
-                    <p className="text-3xl text-[#6f5956]"> {currentFunction}</p>
-                    <p className="text-1xl text-[#6f5956]"> {currentDescription}</p>
+                <div className="flex flex-col w-5/12 h-full gap-4 p-3">
+                    <p className="text-5xl bg-[#a0d5c0] text-white p-4"> {currentName}</p>
+                    <p className="text-3xl text-[#6f5956] mx-2"> {currentFunction}</p>
+                    <p className="text-1xl text-[#6f5956] mx-2"> {currentDescription}</p>
                 </div>
             </div>
             <div className="flex  flex-row w-screen h-1/6 gap-[5%] justify-center">
@@ -57,7 +57,7 @@ const TeamSection = () => {
                                  setCurrentDescription(member.description);
                              }}>
 
-                            <img className="border-2 border-red-950 rounded-2xl" src={member.picture}/>
+                            <img className="border-2 border-[#6f5956] rounded-2xl hover:border-[#a0d5c0] hover:border-4" src={member.picture}/>
                         </div>
                     </div>
                 ))}
