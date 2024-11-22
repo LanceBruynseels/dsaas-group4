352a1f640f3760e84ce140d366a6e8b03d705c54
Manuel Pollet <manuel.pollet@student.kuleuven.be>
2024-11-22 14:22:26 +0100

    Payment page - imporved visuals - (version5)

--- app/payment/page.tsx
+++ app/payment/page.tsx
@@ -4,8 +4,8 @@ import PricingTable from 'components/PricingTable';  // Adjust the import path i
 
 const PricingPage: React.FC = () => {
     return (
-        <div className="container mx-auto py-10 w-screen h-screen">
-            <h1 className="text-center text-3xl font-bold mb-6">Choose Your Plan</h1>
+        <div className="flex flex-col justify-content align-center py-10 w-screen h-screen">
+            <h1 className="text-center text-6xl font-bold mb-6">Choose Your Plan</h1>
             <PricingTable />  {/* This will render the PricingTable component */}
         </div>
     );
