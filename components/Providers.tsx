d216de403ae0e1b600debbb3790322ea78768aab
CantonMonkey <cintauleung@gmail.com>
2024-11-27 22:06:31 +0100

    fixed server-side session management

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,36 +1,36 @@
-'use client';
-
-import { SessionProvider } from "next-auth/react";
-import { ThemeProvider } from "next-themes";
-
-
-// export default function App({
-//                                 Component,
-//                                 pageProps: { session, ...pageProps },
-//                             }) {
+// 'use client';
+//
+// import { SessionProvider } from "next-auth/react";
+// import { ThemeProvider } from "next-themes";
+//
+//
+// // export default function App({
+// //                                 Component,
+// //                                 pageProps: { session, ...pageProps },
+// //                             }) {
+// //     return (
+// //         <SessionProvider session={session}>
+// //             {Component.auth ? (
+// //                 <Auth>
+// //                     <Component {...pageProps} />
+// //                 </Auth>
+// //             ) : (
+// //                 <Component {...pageProps} />
+// //             )}
+// //         </SessionProvider>
+// //     )
+// // }
+// export default function Providers({ children }: { children: React.ReactNode }) {
 //     return (
-//         <SessionProvider session={session}>
-//             {Component.auth ? (
-//                 <Auth>
-//                     <Component {...pageProps} />
-//                 </Auth>
-//             ) : (
-//                 <Component {...pageProps} />
-//             )}
+//         <SessionProvider>
+//             <ThemeProvider
+//                 attribute="class"
+//                 defaultTheme="system"
+//                 enableSystem
+//                 disableTransitionOnChange
+//             >
+//                 {children}
+//             </ThemeProvider>
 //         </SessionProvider>
-//     )
+//     );
 // }
-export default function Providers({ children }: { children: React.ReactNode }) {
-    return (
-        <SessionProvider>
-            <ThemeProvider
-                attribute="class"
-                defaultTheme="system"
-                enableSystem
-                disableTransitionOnChange
-            >
-                {children}
-            </ThemeProvider>
-        </SessionProvider>
-    );
-}
