75b13ca6e7a5f3af2ff9e65ff12b94c2ddff7bd5
Rohan Bhattaram <rohan.bhattaram@student.kuleuven.be>
2024-11-29 12:00:47 +0100

    Mobile version of the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,23 @@
-import React from 'react';
+"use client";
+
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
-import {getServerSession} from "next-auth";
-import { authOptions } from "@/app/api/auth/auth.config";
-import {redirect} from "next/navigation";
-import Link from "next/link";
-import {variableMobile} from "@components/mediaQuery";
 
-export default async function Index() {
+const ResponsiveLayout: React.FC = () => {
+    const [isMobile, setIsMobile] = useState(false);
 
-    const session = await getServerSession(authOptions);
+    useEffect(() => {
+        const mediaQuery = window.matchMedia("(max-width: 640px)");
 
-    if(session){
-        return redirect("/home");
-    }
+        const handleMediaChange = (e: MediaQueryListEvent) => {
+            setIsMobile(e.matches);
+        };
+
+        setIsMobile(mediaQuery.matches);
+        mediaQuery.addEventListener("change", handleMediaChange);
+
+        return () => null;
+    }, []);
 
     return (
         <div className="flex flex-col items-center justify-center min-h-screen text-center px-4 relative">
@@ -20,13 +25,13 @@ export default async function Index() {
                 <Image
                     src="/vlinder.png"
                     alt="Vlinder Logo"
-                    className={`h-auto mx-auto ${variableMobile ? "w-48" : "w-72"}`}
-                    width={variableMobile ? 192 : 288}
-                    height={variableMobile ? 192 : 288}
+                    className={`h-auto mx-auto ${isMobile ? "w-48" : "w-72"}`}
+                    width={isMobile ? 192 : 288}
+                    height={isMobile ? 192 : 288}
                 />
                 <h1
                     className={`${
-                        variableMobile ? "text-3xl" : "text-5xl"
+                        isMobile ? "text-3xl" : "text-5xl"
                     } font-bold text-gray-800 mt-3`}
                 >
                     V(L)INDER
@@ -34,23 +39,22 @@ export default async function Index() {
             </div>
             <div
                 className={`flex ${
-                    variableMobile ? "flex-col space-y-4" : "flex-row space-x-4"
+                    isMobile ? "flex-col space-y-4" : "flex-row space-x-4"
                 } w-full max-w-md mx-auto mt-8`}
             >
                 <a href="http://localhost:3000/sign-in" className="flex-1">
-                    <button
-                        className="w-full bg-[#FCA5A5] hover:bg-[#771d1d] text-white py-3 rounded-xl text-sm transition duration-300">
+                    <button className="w-full bg-[#FCA5A5] hover:bg-[#771d1d] text-white py-3 rounded-xl text-sm transition duration-300">
                         LOGIN
                     </button>
                 </a>
                 <a href="http://localhost:3000/registration" className="flex-1">
-                    <button
-                        className="w-full bg-[#FCA5A5] hover:bg-[#771d1d] text-white py-3 rounded-xl text-sm transition duration-300">
+                    <button className="w-full bg-[#FCA5A5] hover:bg-[#771d1d] text-white py-3 rounded-xl text-sm transition duration-300">
                         REGISTRATIE
                     </button>
                 </a>
             </div>
         </div>
-
     );
-}
+};
+
+export default ResponsiveLayout;
