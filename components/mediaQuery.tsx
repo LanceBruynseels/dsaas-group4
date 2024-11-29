c7213ecaa82319811f37f6e3dafeec52fdceee7f
Rohan Bhattaram <rohan.bhattaram@student.kuleuven.be>
2024-11-29 09:59:48 +0100

    added media query component

---
+++ components/mediaQuery.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import { useState, useEffect } from "react";
+
+export const useIsMobile = () => {
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const mediaQuery = window.matchMedia("(max-width: 640px)");
+
+        const handleMediaChange = (e: MediaQueryListEvent) => {
+            setIsMobile(e.matches);
+        };
+
+        setIsMobile(mediaQuery.matches);
+
+        mediaQuery.addEventListener("change", handleMediaChange);
+
+        return () => mediaQuery.removeEventListener("change", handleMediaChange);
+    }, []);
+
+    return isMobile;
+};
