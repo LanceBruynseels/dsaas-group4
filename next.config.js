4faca1866697a25c5d5ab4150804f8f56430e447
Lance-Bruynseels <101116271+LanceBruynseels@users.noreply.github.com>
2024-12-01 12:27:32 +0100

    forcefully deploy

--- next.config.js
+++ next.config.js
@@ -1,11 +1,4 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-//
-// module.exports = nextConfig;
-//import { i18n } from './next-i18next.config.js';
-
 const nextConfig = {
-    //i18n,
     images: {
         domains: ['cdn.builder.io', 'legfcpyiwzvfhacgnpkw.supabase.co'],
         formats: ['image/avif', 'image/webp'],
