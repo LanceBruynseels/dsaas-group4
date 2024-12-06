57fc940bdecd2717541b5a740658ef17e0fac395
antonceusters24 <antonceusters@gmail.com>
2024-12-06 09:59:26 +0100

    Update UserDisplay.tsx

--- components/UserDisplay.tsx
+++ components/UserDisplay.tsx
@@ -30,7 +30,7 @@ const UserDisplay = () => {
                             href="/api/auth/signout"
                             className="text-xs text-gray-500 hover:text-gray-700"
                         >
-                            Sign out
+                            Log uit
                         </Link>
                     </div>
                     <Image
@@ -47,7 +47,7 @@ const UserDisplay = () => {
                     href="/sign-in"
                     className="text-sm hover:text-primary"
                 >
-                    Sign in
+                    
                 </Link>
             )}
         </div>
