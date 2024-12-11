f3b6cc6f82e39e9492fc47eff96c723542615c05
Manuel Pollet <manuel.pollet@student.kuleuven.be>
2024-12-11 22:31:40 +0100

    paymentPage frontend (v9)

--- app/paymentPage/page.tsx
+++ app/paymentPage/page.tsx
@@ -6,8 +6,7 @@ console.log("paymentPageLoaded")
 
 const PricingPage: React.FC = () => {
     return (
-        <div className="flex flex-col justify-between items-center w-screen h-screen p-16">
-            <h1 className="text-center text-6xl font-bold  ">Choose Your Plan</h1>
+        <div className="flex flex-col justify-between items-center w-screen lg:h-screen h-fit p-4">
             <CreateSubscription />
         </div>
     );
--- components/CreateSubscription.tsx
+++ components/CreateSubscription.tsx
@@ -63,84 +63,85 @@ const ProductsPage = () => {
     };
 
     return (
-        <div className="flex w-screen h-5/6 lg:flex-row flex-col lg:w-full justify-center items-center ">
+        <div className="flex flex-col w-full h-full justify-start items-center gap-8">
+            <h1 className="flex flex-col justify-center items-center bg-red-950 text-white lg:w-[80%] w-[90%] h-[10%] lg:text-[8vh] text-[6vw]">Choose your plan!</h1>
 
-            {product3 && (
-                <div
-                    key={product3.id}
-                    className="flex flex-col justify-center w-1/3 hover:w-2/3 transition-all duration-200 text-center items-center group h-screen  "
-                >
-                    <div
-                        className="flex flex-col justify-between items-center gap-2 p-8 border-white rounded-2xl border-8 w-2/3 group-hover:bg-red-200 h-[66%] group-hover:w-10/12    ">
-                        <h3 className="text-[2vw]">{product3.name}</h3>
-                        <img src="/vlinder.png" alt="Vlinder Logo" className="aspect-square group-hover:hidden"/>
-                        <p className=" group-hover:hidden  text-center text-transparent rounded-2xl p-2 y- "> Best value </p>
-                        <p className="hidden group-hover:flex text-[1.5vw]">{product3.description}</p>
-                        {product3.price && (
-                            <p className="text-[2vw]">
-                                € {product3.price / 100}
-                            </p>
-                        )}
-                        <button
-                            className="hidden group-hover:flex justify-center items-center w-1/3 h-fit p-4 rounded-2xl border-red-950 bg-red-300 hover:bg-red-500 hover:text-white text-[4vh] text-center"
-                            onClick={() => handlePayment(product1.id)}
-                        >
-                            Subscribe
-                        </button>
-                    </div>
-                </div>
-            )}
-            {product2 && (
-                <div
-                    key={product2.id}
-                    className="flex flex-col  justify-center w-1/3 hover:w-2/3 transition-all duration-200 text-center items-center group  h-screen"
-                >
-                    <div
-                        className="flex flex-col justify-between items-center gap-2 p-8 border-white rounded-2xl border-8 w-2/3 group-hover:bg-red-200 group-hover:w-10/12  h-[66%]  ">
-                        <h3 className="text-[2vw]">{product2.name}</h3>
-                        <img src="/vlinder.png" alt="Vlinder Logo" className="aspect-square group-hover:hidden"/>
-                        <p className=" group-hover:hidden  text-center text-transparent rounded-2xl p-2 y- "> Best value </p>
-                        <p className="hidden group-hover:flex text-[1.5vw]">{product2.description}</p>
-                        {product2.price && (
-                            <p className="text-[2vw]">
-                                € {product2.price / 100}
-                            </p>
-                        )}
-                        <button
-                            className="hidden group-hover:flex justify-center items-center w-1/3 h-fit p-4 rounded-2xl border-red-950 bg-red-300 hover:bg-red-500 hover:text-white text-[4vh] text-center"
-                            onClick={() => handlePayment(product1.id)}
-                        >
-                            Subscribe
-                        </button>
-                    </div>
-                </div>
-            )}
-            {product1 && (
-                <div
-                    key={product1.id}
-                    className="flex flex-col  justify-center w-1/3 hover:w-2/3  transition-all duration-200 text-center items-center group  h-screen "
-                >
-                    <div
-                        className="flex flex-col justify-between items-center gap-2 p-8 border-white rounded-2xl border-8 w-2/3 group-hover:bg-red-200 group-hover:w-10/12  h-[66%] ">
-                        <h3 className="text-[2vw]">{product1.name}</h3>
-                        <img src="/vlinder.png" alt="Vlinder Logo" className="aspect-square group-hover:hidden"/>
-                        <p className=" z-50 bg-green-500 text-center text-white rounded-2xl p-2 y- "> Best value </p>
-                        <p className="hidden group-hover:flex text-[1.5vw]">{product1.description}</p>
-                        {product1.price && (
-                            <p className="text-[2vw]">
-                                € {product1.price / 100}
-                            </p>
-                        )}
-                        <button
-                            className="hidden group-hover:flex justify-center items-center w-1/3 h-fit p-4 rounded-2xl border-red-950 bg-red-300 hover:bg-red-500 hover:text-white text-[4vh] text-center"
-                            onClick={() => handlePayment(product1.id)}
-                        >
-                            Subscribe
-                        </button>
-                    </div>
-                </div>
-            )}
+            <div className="flex lg:flex-row flex-col justify-center items-center gap-4 w-full lg:aspect-[4/1] aspect[1/4]">
 
+                {product3 && (
+                    <div
+                        key={product3.id}
+                        className="flex flex-col justify-center items-center lg:w-[25%] w-[80%] transition-all duration-200 text-center group aspect-[1/1] lg:hover:aspect-[3/2] lg:hover:w-[40%] "
+                    >
+                        <div className="flex flex-col justify-between items-center gap-[1%] p-[5%] border-white rounded-2xl border-8 w-[90%] group-hover:bg-red-200 h-[90%] lg:group-hover:w-[90%]">
+                            <h3 className="text-[2vw] h-[15%]">{product3.name}</h3>
+                            <img src="/vlinder.png" alt="Vlinder Logo" className="aspect-square group-hover:hidden h-[50%]"/>
+                            <p className=" group-hover:hidden  text-center text-transparent rounded-2xl p-2  text-[1vh]"> Best value </p>
+                            <p className="hidden group-hover:flex text-[1.5vw]">{product3.description}</p>
+                            {product3.price && (
+                                <p className="lg:text-[2vw] text-[4vw] h-[15%] w-full">
+                                    € {product3.price / 100}
+                                </p>
+                            )}
+                            <button
+                                className="hidden group-hover:flex justify-center items-center h-[10%] aspect-[3/1] p-4 rounded-2xl border-red-950 bg-red-300 hover:bg-red-500 hover:text-white text-[2vh] text-center"
+                                onClick={() => handlePayment(product3.id)}
+                            >
+                                Subscribe
+                            </button>
+                        </div>
+                    </div>
+                )}
+                {product2 && (
+                    <div
+                        key={product2.id}
+                        className="flex flex-col justify-center items-center lg:w-[25%] w-[80%] transition-all duration-200 text-center group aspect-[1/1] lg:hover:aspect-[3/2] lg:hover:w-[40%] "
+                    >
+                    <div className="flex flex-col justify-between items-center gap-[1%] p-[5%] border-white rounded-2xl border-8 w-[90%] group-hover:bg-red-200 h-[90%] lg:group-hover:w-[90%]">
+                            <h3 className="text-[2vw] h-[15%]">{product2.name}</h3>
+                            <img src="/vlinder.png" alt="Vlinder Logo" className="aspect-square group-hover:hidden h-[50%]"/>
+                            <p className=" group-hover:hidden  text-center text-transparent rounded-2xl p-2  text-[1vh] "> Best value </p>
+                            <p className="hidden group-hover:flex text-[1.5vw]">{product2.description}</p>
+                            {product2.price && (
+                                <p className="lg:text-[2vw] text-[4vw] h-[15%] w-full">
+                                    € {product2.price / 100}
+                                </p>
+                            )}
+                            <button
+                                className="hidden group-hover:flex justify-center items-center h-[10%] aspect-[3/1] p-4 rounded-2xl border-red-950 bg-red-300 hover:bg-red-500 hover:text-white text-[2vh] text-center"
+                                onClick={() => handlePayment(product2.id)}
+                            >
+                                Subscribe
+                            </button>
+                        </div>
+                    </div>
+                )}
+                {product1 && (
+                    <div
+                        key={product1.id}
+                        className="flex flex-col justify-center items-center lg:w-[25%] w-[80%] transition-all duration-200 text-center group aspect-[1/1] lg:hover:aspect-[3/2] lg:hover:w-[40%] "
+                    >
+                    <div className="flex flex-col justify-between items-center gap-[1%] p-[5%] border-white rounded-2xl border-8 w-[90%] group-hover:bg-red-200 h-[90%] lg:group-hover:w-[90%]">
+                            <h3 className="text-[2vw] h-[15%]">{product1.name}</h3>
+                            <img src="/vlinder.png" alt="Vlinder Logo" className="aspect-square group-hover:hidden h-[50%]"/>
+                            <p className=" text-center text-white rounded-2xl p-2 bg-green-600 text-[1vh] "> Best value </p>
+                            <p className="hidden group-hover:flex text-[1.5vw]">{product1.description}</p>
+                            {product1.price && (
+                                <p className="lg:text-[2vw] text-[4vw] h-[15%] w-full">
+                                    € {product1.price / 100}
+                                </p>
+                            )}
+                            <button
+                                className="hidden group-hover:flex justify-center items-center h-[10%] aspect-[3/1] p-4 rounded-2xl border-red-950 bg-red-300 hover:bg-red-500 hover:text-white text-[2vh] text-center"
+                                onClick={() => handlePayment(product1.id)}
+                            >
+                                Subscribe
+                            </button>
+                        </div>
+                    </div>
+                )}
+
+            </div>
         </div>
     );
 };
