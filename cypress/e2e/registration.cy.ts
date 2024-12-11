97df987f81cac8a2a1a2268013d05720197eb1b9
Rohan Bhattaram <rohan.bhattaram@student.kuleuven.be>
2024-12-11 14:34:01 +0100

    tests

--- cypress/e2e/registration.cy.ts
+++ cypress/e2e/registration.cy.ts
@@ -1,18 +1,7 @@
-it('should display the logo and registration form', () => {
-    cy.visit('https://dsaas-group4-5dhkwhosn-lance-bruynseels-projects.vercel.app/registration')
-
-    cy.get('img[alt="Vlinder Logo"]').should('be.visible');
-
-    cy.contains('Registreer').should('be.visible');
-    cy.get('input[name="username"]').should('be.visible');
-    cy.get('input[name="password"]').should('be.visible');
-    cy.get('select[name="facility"]').should('be.visible');
-    cy.get('select[name="supervisor"]').should('be.visible');
-    cy.contains('Registreer').should('be.visible');
-});
-
 
 it('should allow filling out the form', () => {
-    cy.visit('https://dsaas-group4-5dhkwhosn-lance-bruynseels-projects.vercel.app/registration')
-    cy.contains('Registreer').click();
+    cy.visit('https://localhost:3000/')
+    cy.contains('REGISTRATIE').click();
 });
+
+
--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,59 +1,63 @@
-// describe('My First Test', () => {
-//   it('Does not do much!', () => {
-//     expect(true).to.equal(true)
-//   })
-// })
+describe(' Page Test', () => {
+  it('Fills out and submits the registration form', () => {
+    cy.visit('http://localhost:3000/registration');
+    cy.get('input').eq(0).type('TestUser');
 
-// describe('My First Test', () => {
-//   it('Does not do much!', () => {
-//     expect(true).to.equal(false)
-//   })
-// })
-//
-// describe('My First Test', () => {
-//   it('Visits the Kitchen Sink', () => {
-//     cy.visit('https://example.cypress.io')
-//   })
-// })
+    cy.get('input').eq(1).type('SecurePassword123');
 
-// describe('My First Test', () => {
-//   it('finds the content "type"', () => {
-//     cy.visit('https://example.cypress.io')
-//
-//     cy.contains('type')
-//   })
-// })
+    cy.get('select').eq(0).select('faciliteit Leuven');
 
-// describe('My First Test', () => {
-//   it('clicks the link "type"', () => {
-//     cy.visit('https://example.cypress.io')
-//
-//     cy.contains('type').click()
-//   })
-// })
+    cy.get('select').eq(1).select('Jan');
 
-// describe('Cypress test', () => {
-//   it('clicking database.new ', () => {
-//     cy.visit('https://dsaas-group4.vercel.app/')
-//
-//     cy.contains('database.new').click()
-//
-//   })
-// })
-
-describe('Landing Page test', () => {
-  it('Gets, types and asserts', () => {
-    cy.visit('http://localhost:3000')
-
-    cy.contains('registratie').click()
-
-
-    cy.url().should('include', '/registration')
-
-    cy.get('.Gebruikersnaam').type('')
-
-    cy.get('.action-email').should('have.value', 'fake@email.com')
-  })
+    cy.contains('Registreer').click();
+  });
 })
+
+describe('All Other Pages Test', () => {
+  it('Fills out and submits the login form', () => {
+    cy.visit('http://localhost:3000/');
+    cy.wait(2000);
+    cy.contains('LOGIN').click();
+    cy.url().should('include', '/sign-in');
+    cy.wait(1000);
+    cy.get('input[name="username"]').type('pepemon');
+    cy.get('input[name="password"]').type('123+');
+    cy.wait(1000);
+    cy.contains('Log in').then(($btn) => {
+      cy.wrap($btn).click().then(() => {
+        cy.url().should('include', '/home');
+      });
+    });
+
+    cy.url().should('include','/home');
+    cy.wait(1000);
+    cy.contains('Berichten').click();
+    cy.url().should('include','/messaging');
+    cy.wait(4000);
+    cy.contains('pepemon2').click();
+    cy.wait(1000);
+    cy.get('TextArea[placeholder="Type your message..."]').type('Yo from cypress');
+    cy.wait(1000);
+    cy.get('.bg-blue-500').click();
+    cy.contains('Yo from cypress');
+    cy.contains('Ontdek').click();
+    cy.wait(1000);
+    cy.url().should('include','/ontdek');
+    cy.wait(5000);
+    cy.get('input[placeholder="Search chats"]').type('Films');
+
+    cy.contains('Search').click();
+    cy.wait(6000);
+    cy.get('button').contains('Films/Cinema').click();
+    cy.url().should('include', '/group-messaging');
+    cy.get('TextArea[placeholder="Type your message..."]').type('I like films: from cypress');
+
+  });
+});
+
+
+
+
+
 
 
