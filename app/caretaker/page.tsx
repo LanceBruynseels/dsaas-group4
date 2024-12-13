9b30695d06ad04f02ae823aa7129af49c5acc532
Lance-Bruynseels <101116271+LanceBruynseels@users.noreply.github.com>
2024-12-13 14:34:10 +0100

    bugfixes

--- app/caretaker/page.tsx
+++ app/caretaker/page.tsx
@@ -21,8 +21,7 @@ export default async function Index() {
                     </button>
                 </Link>
                 <Link href="/sign-up-caretaker" className="flex-1">
-                    <button
-                        className="w-full bg-[#FCA5A5] hover:bg-[#771d1d] text-white py-2 rounded-xl transition duration-300">
+                    <button className="w-full bg-[#FCA5A5] hover:bg-[#771d1d] text-white py-2 rounded-xl transition duration-300">
                         REGISTRATIE
                     </button>
                 </Link>
--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -40,13 +40,13 @@ export const updateSession = async (request: NextRequest) => {
     const user = await supabase.auth.getUser();
 
     // protected routes
-    if (request.nextUrl.pathname.startsWith("/home") && user.error) {
-      return NextResponse.redirect(new URL("/sign-in", request.url));
-    }
+    // if (request.nextUrl.pathname.startsWith("/protected") && user.error) {
+    //   return NextResponse.redirect(new URL("/sign-in", request.url));
+    // }
 
-    if (request.nextUrl.pathname === "/home" && !user.error) {
-      return NextResponse.redirect(new URL("/", request.url));
-    }
+    // if (request.nextUrl.pathname === "/home" && !user.error) {
+    //   return NextResponse.redirect(new URL("/", request.url));
+    // }
 
     return response;
   } catch (e) {
